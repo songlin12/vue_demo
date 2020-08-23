baf361a9856fc29ee458b879834fcf80559169b7
天明 <182442556@qq.com>
2020-08-23 23:12:25 +0800

    test

--- src/views/Child.vue
+++ src/views/Child.vue
@@ -1,40 +1,39 @@
 <template>
-    <div>
-        <h3>Children</h3>
-        <h5>{{msg}}</h5>
-        <h6>{{childMsg}}</h6>
-        <button @click="passMsg">走你！</button>
-    </div>
+  <div>
+    <h3>Children</h3>
+    <h5>{{msg}}</h5>
+    <h6>{{childMsg}}</h6>
+    <button @click="passMsg">走你！</button>
+  </div>
 </template>
 
 <script>
-import bus from '../util/bus'
-    export default {
-        props: {
-            msg: {
-                type: String,
-                default: ''
-            },
-        },
-        data() {
-            return {
-                childMsg: 'child msg'
-            }
-        },
-        methods: {
-            passMsg() {
-                this.$emit('showMsg','i am from CHild')
-            }
-        },
-        mounted() {
-            console.log('attrs',this.$attrs)
-            bus.$on('msg',(val) => {
-                this.childMsg = val
-            });
-        },  
-    }
+import bus from "../util/bus";
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      childMsg: "child msg",
+    };
+  },
+  methods: {
+    passMsg() {
+      this.$emit("showMsg", "i am from CHild");
+    },
+  },
+  mounted() {
+    console.log("attrs", this.$attrs);
+    bus.$on("msg", (val) => {
+      this.childMsg = val;
+    });
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
